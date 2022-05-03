7668a2dce9e9ae3616be55c2d97fff85025e821c
rhydb <hello@rhydb.com>
2022-05-03 13:23:42 +0100

    remove unused 'entflipped'

--- entity.py
+++ entity.py
@@ -8,7 +8,6 @@ class entity():
         self.size = 48
         self.ent = pygame.image.load(os.path.join("Assets", imagedirec))
         self.ent = pygame.transform.scale(self.ent, (self.size, self.size))
-        self.entfliped = pygame.transform.flip(self.ent, True, False)
         self.position = Vector2(position)
         self.acceleration = acceleration
         self.velocity = Vector2(0, 0)
