27c5777e2d90c79dc571ff6b4cbcce68ee4fc275
rhydb <hello@rhydb.com>
2022-05-07 17:59:42 +0100

    change jump and acceleration

--- player.py
+++ player.py
@@ -6,9 +6,9 @@ class Player(Entity):
     def __init__(self, name, acceleration, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.min_speed = 5
-        self.max_vel = Vector2(250, 500)
+        self.max_vel = Vector2(250, 600)
         self.name = name
         self.acceleration = Vector2(acceleration)
         self.deceleration = 10
         self.gravity = 2000
-        self.jump = 500
+        self.jump = 600
